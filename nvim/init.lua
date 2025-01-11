ae104ba9c759641bb705b2d3a7927e88d6d38b9a
Alisa Valiullina <avaliull@f0r3s6.codam.nl>
2025-01-11 19:04:03 +0100

    tokyonight, treesitter, still to fix highlighting

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,21 @@
 require("config.lazy")              -- runs config.lazy on startup
 require("lazy").setup("plugins")    -- something forr plugin setup
+require("settings")		-- runs settings
+
+-- PLUGINS --
+
+--	treesitter	--
+require("lazy").setup({{
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "c", "cpp", "make", "rust", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+ }})
---
+++ nvim/lua/settings.lua
@@ -0,0 +1,9 @@
+local global = vim.g
+local o = vim.opt
+
+o.number = true -- Print the line number in front of each line
+o.relativenumber = true -- Show the line number relative to the line with t
+o.ruler = true -- Show the line and column number of the cursor position, separated by a comma.o
+o.showmatch = true -- When a bracket is inserted, briefly jump to the matc
+o.title = true -- When on, the title of the window will be set to the value of 'titlestring'
+vim.cmd[[colorscheme tokyonight-night]]
