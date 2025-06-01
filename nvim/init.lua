630b631686a273c13eebb195abd5a553c42cbf49
Alisa Valiullina <avaliull@f0r2s2.codam.nl>
2025-06-01 18:11:08 +0200

    changed some settings around for more sensible structure

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,3 @@
+require("config.settings")					-- runs settings
 require("config.lazy")              -- runs config.lazy on startup
 require("lazy").setup("plugins")    -- grabs all plugins from the plugins folder and runs them from lazy
-require("settings")					-- runs settings
--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,13 +18,12 @@ vim.opt.rtp:prepend(lazypath)
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = ";"
-vim.g.maplocalleader = "\\"
 
 --  -- colorscheme that will be used when installing plugins.
-install = { colorscheme = { "habamax" } }
--- automatically check for plugin updates
-checker = { enabled = true }
+--install = { colorscheme = { "habamax" } }
+--
+---- automatically check for plugin updates
+--checker = { enabled = true }
 
 ---- Setup lazy.nvim
 --require("lazy").setup({
