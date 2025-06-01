7bfe9630df53d675ef627bfe6d31eda422ae8bdb
Alisa Valiullina <avaliull@f0r2s2.codam.nl>
2025-06-01 19:35:21 +0200

    new plugins

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,2 +1,17 @@
 local builtin = require('telescope.builtin')
+local telescope = require('telescope')
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files,{})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep,{})
+vim.keymap.set('n', '<leader>fb', builtin.buffers,{})
+
+telescope.setup({
+	defaults = {
+		layout_config = {
+		  vertical = {
+		    prompt_position = "top",
+		    mirror = true,
+	 		},
+		},
+	},
+})
--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -1,9 +1,9 @@
-return	
+return
 {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
 	config = function()
-		vim.cmd("colorscheme tokyonight-night")
+		vim.cmd("colorscheme tokyonight-moon")
 	end,
 }
